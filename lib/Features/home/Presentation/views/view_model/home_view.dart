15e6cbd8909813d04c5bd04900e255fffc54e00b
Ahmed Safwat <a01273477051@gmail.com>
2023-02-20 18:06:28 +0200

    add custom app bar to home page 🔍✨

--- lib/Features/home/Presentation/views/view_model/home_view.dart
+++ lib/Features/home/Presentation/views/view_model/home_view.dart
@@ -1,11 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:my_bookly/Features/home/Presentation/views/view_model/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: HomeViewBody(),
+    );
   }
 }
---
+++ lib/Features/home/Presentation/views/view_model/widgets/custom_app_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:my_bookly/core/utils/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(
+            LocAssets.logo,
+            height: 18,
+          ),
+          const Spacer(),
+          IconButton(
+            alignment: Alignment.bottomCenter,
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 24,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
