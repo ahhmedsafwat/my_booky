92bf828f666c6cc08751d10af4972183d2785a8c
Ahmed Safwat <a01273477051@gmail.com>
2023-02-20 14:16:12 +0200

    added the underline text

--- lib/Features/Splash/Presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/Presentation/views/widgets/splash_view_body.dart
@@ -11,6 +11,18 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(LocAssets.logo),
+        const SizedBox(
+          height: 4,
+        ),
+        const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 22,
+            letterSpacing: 3,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData().copyWith(scaffoldBackgroundColor: KMainColor),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: KMainColor),
       home: const SplashView(),
     );
   }
