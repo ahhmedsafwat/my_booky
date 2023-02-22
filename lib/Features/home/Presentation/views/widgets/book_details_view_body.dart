f8453deb6859c84ad5cc67ab53dd00992c3fc771
Ahmed Safwat <a01273477051@gmail.com>
2023-02-22 15:13:19 +0200

    add title and subtitle to details view

--- lib/Features/home/Presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/Presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_bookly/Features/home/Presentation/views/widgets/custom_book_item.dart';
+import 'package:my_bookly/constent.dart';
+import 'package:my_bookly/core/utils/style.dart';
 import 'custom_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -14,9 +16,29 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .18),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const CustomBookItem(),
           ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text(
+            'The Jungle Book',
+            style: Style.textStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Style.textStyle18.copyWith(
+                fontWeight: FontWeight.w500,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          )
         ],
       ),
     );
--- lib/core/utils/style.dart
+++ lib/core/utils/style.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:my_bookly/constent.dart';
 
 abstract class Style {
-  static const textStyle18 = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w600,
-  );
-  static const textStyle20 = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.normal,
-  );
   static const textStyle14 = TextStyle(
     fontWeight: FontWeight.normal,
     fontSize: 14,
   );
+
   static const textStyle16 = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 16,
   );
+
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+
   static const textStyle30 = TextStyle(
     fontSize: 30,
-    fontWeight: FontWeight.bold,
+    fontWeight: FontWeight.normal,
+    fontFamily: KGtSectraFine,
   );
 }
